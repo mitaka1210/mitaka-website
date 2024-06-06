a8543eaf813c361fe957ca8163afd59576b6822c
Dimitar <dimitar_dimitrov12@mail.bg>
2024-06-06 11:59:19 +0300

    add css
    add css

--- src/app/Projects-page/ProjectsHTML.jsx
+++ src/app/Projects-page/ProjectsHTML.jsx
@@ -1,9 +1,11 @@
+'use client'
+
 import React from 'react';
 import './project.scss';
 const ProjectsHtml = () => {
   return (
-    <div>
-      <section className='start-programing'>
+    <div className="flex-vertical-container text-align-center justify-content-center">
+      <section className='start-programing flex-item'>
       <h2>Така започна всичко и още много.</h2>
       <div className="box-wrapper">
         <figure className="shape-box shape-box_half">
@@ -18,7 +20,7 @@ const ProjectsHtml = () => {
             </div>
             <p className="card-content">Приложение чрез което може да получавате информация за време. Нужно е само да
               въведете град. Може да бъде инсталирано на телефона.</p>
-            <h5>Технологий</h5>
+            <h5 className='margin-top-5 border-color-bottom-gray'>Технологий</h5>
             <ul className="card-content">
               <li>HTML</li>
               <li>SCSS</li>
@@ -27,8 +29,10 @@ const ProjectsHtml = () => {
               <li>Jekyll</li>
               <li>Figma</li>
             </ul>
-            <a href="#" className="read-more-btn">Приложение</a>
-            <a href="#" className="read-more-btn">Код в GitHub</a>
+            <div className='buttons-wrapper flex-horizontal-container justify-content-end text-align-center align-items-center'>
+              <a href="#" className="read-more-btn">Приложение</a>
+              <a href="#" className="read-more-btn">Код в GitHub</a>
+            </div>
           </figcaption>
           <span className="after"></span>
         </figure>
@@ -42,21 +46,25 @@ const ProjectsHtml = () => {
               <h4 className="card-main-title">React website</h4>
             </div>
             <p className="card-content">Страници с оформление направени с ReactJs and Figma. Адаптивен дизайн</p>
-            <h5>Технологий</h5>
+            <h5 className='margin-top-5 border-color-bottom-gray'>Технологий</h5>
             <ul className="card-content">
               <li>HTML</li>
               <li>SCSS</li>
               <li>JS</li>
               <li>ReactJS</li>
             </ul>
-            <a href="#" className="read-more-btn">Приложение</a>
-            <a href="#" className="read-more-btn">Код в GitHub</a>
+            <div
+              className='buttons-wrapper flex-horizontal-container justify-content-end text-align-center align-items-center'>
+              <a href="#" className="read-more-btn">Приложение</a>
+              <a href="#" className="read-more-btn">Код в GitHub</a>
+            </div>
           </figcaption>
           <span className="after"></span>
         </figure>
         <figure className="shape-box shape-box_half">
           <img
-            src="https://images.unsplash.com/photo-1498075702571-ecb018f3752d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=757&q=80" alt='logo'/>
+            src="https://images.unsplash.com/photo-1498075702571-ecb018f3752d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=757&q=80"
+            alt='logo'/>
           <div className="brk-abs-overlay z-index-0 bg-black opacity-60"></div>
           <figcaption>
             <div className="show-cont">
@@ -65,15 +73,18 @@ const ProjectsHtml = () => {
             </div>
             <p className="card-content">Приложение с което можете да си записвате задачите и да го променяте. Адаптивен
               дизайн</p>
-            <h5>Технологий</h5>
+            <h5 className='margin-top-5 border-color-bottom-gray'>Технологий</h5>
             <ul className="card-content">
               <li>HTML</li>
               <li>SCSS</li>
               <li>JS</li>
               <li>ReactJS</li>
             </ul>
-            <a href="#" className="read-more-btn">Приложение</a>
-            <a href="#" className="read-more-btn">Код в GitHub</a>
+            <div
+              className='buttons-wrapper flex-horizontal-container justify-content-end text-align-center align-items-center'>
+              <a href="#" className="read-more-btn">Приложение</a>
+              <a href="#" className="read-more-btn">Код в GitHub</a>
+            </div>
           </figcaption>
           <span className="after"></span>
         </figure>
@@ -87,52 +98,55 @@ const ProjectsHtml = () => {
               <h4 className="card-main-title">React website</h4>
             </div>
             <p className="card-content">Страници с оформление направени с ReactJs and Figma. Адаптивен дизайн</p>
-            <h5>Технологий</h5>
+            <h5 className='margin-top-5 border-color-bottom-gray'>Технологий</h5>
             <ul className="card-content">
               <li>HTML</li>
               <li>SCSS</li>
               <li>JS</li>
               <li>ReactJS</li>
             </ul>
-            <a href="#" className="read-more-btn">Приложение</a>
-            <a href="#" className="read-more-btn">Код в GitHub</a>
+            <div
+              className='buttons-wrapper flex-horizontal-container justify-content-end text-align-center align-items-center'>
+              <a href="#" className="read-more-btn">Приложение</a>
+              <a href="#" className="read-more-btn">Код в GitHub</a>
+            </div>
           </figcaption>
           <span className="after"></span>
         </figure>
       </div>
       </section>
-      <section className='upgrade-my-self'>
-      <h2>Надграждам себе си!</h2>
-      <div className="box-wrapper">
-        <figure className="shape-box shape-box_half">
-          <img
-            src="https://images.unsplash.com/photo-1534670007418-fbb7f6cf32c3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
+      <section className='upgrade-my-self flex-item'>
+        <h2>Надграждам себе си!</h2>
+        <div className="box-wrapper">
+          <figure className="shape-box shape-box_half">
+            <img
+              src="https://images.unsplash.com/photo-1534670007418-fbb7f6cf32c3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
             alt=""/>
           <div className="brk-abs-overlay z-index-0 bg-black opacity-60"></div>
-          <figcaption>
-            <div className="show-cont">
-              <h3 className="card-no">05</h3>
-              <h4 className="card-main-title">Начална страница</h4>
-            </div>
-            <p className="card-content">Приложение чрез което може да получавате информация за време. Нужно е само да
-              въведете град. Може да бъде инсталирано на телефона.</p>
-            <h5>Технологий</h5>
-            <ul className="card-content">
-              <li>HTML</li>
-              <li>SCSS</li>
-              <li>JS</li>
-              <li>Gulp</li>
-              <li>Jekyll</li>
-              <li>Figma</li>
-            </ul>
-            <a href="#" className="read-more-btn">Приложение</a>
-            <a href="#" className="read-more-btn">Код в GitHub</a>
-          </figcaption>
-          <span className="after"></span>
-        </figure>
-        <figure className="shape-box shape-box_half">
-          <img
-            src="https://images.unsplash.com/photo-1512295767273-ac109ac3acfa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=375&q=80"/>
+            <figcaption>
+              <div className="show-cont">
+                <h3 className="card-no">05</h3>
+                <h4 className="card-main-title">Невероятни обувки <span> (Начална страница)</span></h4>
+              </div>
+              <p className="card-content">Приложение чрез което може да получавате информация за време. Нужно е само да
+                въведете град. Може да бъде инсталирано на телефона.</p>
+              <h5 className='margin-top-5 border-color-bottom-gray'>Технологий</h5>
+              <ul className="card-content">
+                <li>HTML</li>
+                <li>SCSS</li>
+                <li>Figma</li>
+              </ul>
+              <div
+                className='buttons-wrapper flex-horizontal-container justify-content-end text-align-center align-items-center'>
+                <a href="#" className="read-more-btn">Приложение</a>
+                <a href="#" className="read-more-btn">Код в GitHub</a>
+              </div>
+            </figcaption>
+            <span className="after"></span>
+          </figure>
+          <figure className="shape-box shape-box_half">
+            <img
+              src="https://images.unsplash.com/photo-1512295767273-ac109ac3acfa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=375&q=80" alt='logo-6'/>
           <div className="brk-abs-overlay z-index-0 bg-black opacity-60"></div>
           <figcaption>
             <div className="show-cont">
@@ -140,12 +154,12 @@ const ProjectsHtml = () => {
               <h4 className="card-main-title">Платежен лист (TypeScript)</h4>
             </div>
             <p className="card-content">Страници с оформление направени с ReactJs and Figma. Адаптивен дизайн</p>
-            <h5>Технологий</h5>
+            <h5 className='margin-top-5 border-color-bottom-gray'>Технологий</h5>
             <ul className="card-content">
               <li>HTML</li>
               <li>SCSS</li>
               <li>JS</li>
-              <li>ReactJS</li>
+              <li>TypeScript</li>
             </ul>
             <a href="#" className="read-more-btn">Приложение</a>
             <a href="#" className="read-more-btn">Код в GitHub</a>
@@ -154,60 +168,68 @@ const ProjectsHtml = () => {
         </figure>
         <figure className="shape-box shape-box_half">
           <img
-            src="https://images.unsplash.com/photo-1498075702571-ecb018f3752d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=757&q=80" alt='logo'/>
+            src="https://images.unsplash.com/photo-1498075702571-ecb018f3752d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=757&q=80" alt='logo-7'/>
           <div className="brk-abs-overlay z-index-0 bg-black opacity-60"></div>
           <figcaption>
             <div className="show-cont">
               <h3 className="card-no">07</h3>
-              <h4 className="card-main-title">Пица магазин</h4>
+              <h4 className="card-main-title">Пица меню</h4>
             </div>
             <p className="card-content">Приложение с което можете да си записвате задачите и да го променяте. Адаптивен
               дизайн</p>
-            <h5>Технологий</h5>
+            <h5 className='margin-top-5 border-color-bottom-gray'>Технологий</h5>
             <ul className="card-content">
               <li>HTML</li>
               <li>SCSS</li>
               <li>JS</li>
               <li>ReactJS</li>
+              <li>Material-UI</li>
+              <li>Redux</li>
+              <li>Styled-components</li>
             </ul>
-            <a href="#" className="read-more-btn">Приложение</a>
-            <a href="#" className="read-more-btn">Код в GitHub</a>
-          </figcaption>
-          <span className="after"></span>
-        </figure>
-        <figure className="shape-box shape-box_half">
-          <img
-            src="https://images.unsplash.com/photo-1534669740902-e09e38a6a29f?ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"/>
-          <div className="brk-abs-overlay z-index-0 bg-black opacity-60"></div>
-          <figcaption>
-            <div className="show-cont">
-              <h3 className="card-no">08</h3>
-              <h4 className="card-main-title">Електронен магазин</h4>
+            <div
+              className='buttons-wrapper flex-horizontal-container justify-content-end text-align-center align-items-center'>
+              <a href="#" className="read-more-btn">Приложение</a>
+              <a href="#" className="read-more-btn">Код в GitHub</a>
             </div>
-            <p className="card-content">Страници с оформление направени с ReactJs and Figma. Адаптивен дизайн</p>
-            <h5>Технологий</h5>
-            <ul className="card-content">
-              <li>HTML</li>
-              <li>SCSS</li>
-              <li>JS</li>
-              <li>ReactJS</li>
-            </ul>
-            <a href="#" className="read-more-btn">Приложение</a>
-            <a href="#" className="read-more-btn">Код в GitHub</a>
           </figcaption>
           <span className="after"></span>
         </figure>
-      </div>
+          <figure className="shape-box shape-box_half">
+            <img
+              src="https://images.unsplash.com/photo-1534669740902-e09e38a6a29f?ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80" alt='logo-8'/>
+            <div className="brk-abs-overlay z-index-0 bg-black opacity-60"></div>
+            <figcaption>
+              <div className="show-cont">
+                <h3 className="card-no">08</h3>
+                <h4 className="card-main-title">Електронен магазин</h4>
+              </div>
+              <p className="card-content">Страници с оформление направени с ReactJs and Figma. Адаптивен дизайн</p>
+              <h5 className='margin-top-5 border-color-bottom-gray'>Технологий</h5>
+              <ul className="card-content">
+                <li>HTML</li>
+                <li>SCSS</li>
+                <li>JS</li>
+                <li>ReactJS</li>
+              </ul>
+              <div
+                className='buttons-wrapper flex-horizontal-container justify-content-end text-align-center align-items-center'>
+                <a href="#" className="read-more-btn">Приложение</a>
+                <a href="#" className="read-more-btn">Код в GitHub</a>
+              </div>
+            </figcaption>
+            <span className="after"></span>
+          </figure>
+        </div>
       </section>
-
-      <section className="magic-novatio flex-horizontal-container justify-content-center text-align-center">
+      <section className="magic-novatio flex-vertical-container justify-content-center text-align-center">
         <h2>Продължавам да се развивам и надграждам себе си!</h2>
         <div>магията<strong>Новатио</strong></div>
         <div className="box-wrapper">
           <figure className="shape-box shape-box_half">
             <img
               src="https://images.unsplash.com/photo-1534670007418-fbb7f6cf32c3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
-              alt=""/>
+              alt="logo-9"/>
             <div className="brk-abs-overlay z-index-0 bg-black opacity-60"></div>
             <figcaption>
               <div className="show-cont">
@@ -216,7 +238,7 @@ const ProjectsHtml = () => {
               </div>
               <p className="card-content">Приложение чрез което може да получавате информация за време. Нужно е само да
                 въведете град. Може да бъде инсталирано на телефона.</p>
-              <h5>Технологий</h5>
+              <h5 className='margin-top-5 border-color-bottom-gray'>Технологий</h5>
               <ul className="card-content">
                 <li>HTML</li>
                 <li>SCSS</li>
@@ -225,8 +247,11 @@ const ProjectsHtml = () => {
                 <li>Vue</li>
                 <li>RxJs</li>
               </ul>
-              <a href="#" className="read-more-btn">Приложение</a>
-              <a href="#" className="read-more-btn">Код в GitHub</a>
+              <div
+                className='buttons-wrapper flex-horizontal-container justify-content-end text-align-center align-items-center'>
+                <a href="#" className="read-more-btn">Приложение</a>
+                <a href="#" className="read-more-btn">Код в GitHub</a>
+              </div>
             </figcaption>
             <span className="after"></span>
           </figure>
