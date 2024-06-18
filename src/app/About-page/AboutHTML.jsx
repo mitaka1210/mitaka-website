0e9073ab2f7cbd2c76ac294b2bd2dd2efa2969c2
Dimitar <dimitar_dimitrov12@mail.bg>
2024-06-18 11:24:30 +0300

    remove no used css and translate about page

--- src/app/About-page/AboutHTML.jsx
+++ src/app/About-page/AboutHTML.jsx
@@ -3,10 +3,14 @@ import React, {forwardRef, useState} from "react";
 import {accordian} from "@/content";
 import withClickOutside from "../Helper-components/Click-outside/WithClickOutSide";
 import ScrollTop from "@/app/Helper-components/scrollToTop/scrollTop";
+import {useTranslation} from "react-i18next";
 
 const AboutHtml = forwardRef(({open, setOpen}, ref) => {
+  // imported from library
   const [active, setActive] = useState(-1);
   const [hideDiv, setHideDiv] = useState(true);
+  const {t} = useTranslation();
+  // methods
   const handleClick = (index) => {
     if (index === active) {
       setActive(-1);
@@ -26,21 +30,17 @@ const AboutHtml = forwardRef(({open, setOpen}, ref) => {
 
   return (
     <div className="about">
-      <div className="about-me">
-        <h2 className="text-align-center">За мен</h2>
+      <div
+        className="about-me text-align-center flex-horizontal-container-raw justify-content-center align-items-center">
+        <h2 className="">{t("aboutMe")}</h2>
       </div>
       <section>
 
-        <div className="my-hobbi" onClick={closeDiv}>
-          <p className="about-me-text margin-15">Здравейте, казвам се Димитър Димитров и съм авиационен инженер. Но
-            с времето се наложи да се преклалифицирам в моето хоби към този момент.Именно в програмирането и по
-            специално в Front-end частта.
-            Страстен съм към различни хобита,лични финанси, аквариумистика, проекти с Arduino, разходки в
-            природата, готварство, риболов, програмиране, четене и инвестиране.</p>
-          <p className="about-me-text"> Моите цели са да постигна финансова независимост и да стана качествен
-            програмист.
-            Известен съм със своята упоритост, говоря директно това което мисля и съм в готовност да помагам на
-            другите.</p>
+        <div
+          className="my-hobbi flex-vertical-container justify-content-center align-items-center text-align-center margin-15"
+          onClick={closeDiv}>
+          <span className="about-me-text margin-15">{t("whoIam")}</span>
+          <span className="about-me-text">{t("goals")}</span>
           <ul className=" text-align-justify accordian">
             {accordian.map((item, index) => {
               return (
@@ -69,21 +69,20 @@ const AboutHtml = forwardRef(({open, setOpen}, ref) => {
         </div>
         <div className="text-align-center maxWidthAndHeight margin-bottom-30 my-motto">
           <h4>
-            <q>Единственият начин да се подобрите е да приемете провалa.</q>
+            <q>{t("motoOne")}</q>
           </h4>
           <hr className="add-line-bottom"/>
         </div>
       </section>
-      <div className="text-align-center my-focus"><h2>В момента съм фокусиран върху</h2></div>
+      <div className="text-align-center my-focus"><h4>{t("myFocus")}</h4></div>
       <section className="info-bout-me flex-horizontal-container">
         <div className="box-3">
           <div className="overlay-box round-behavior shadowed-element border-color-gray-2px">
             <div
               className="desc flex-vertical-container-raw justify-content-center align-items-center text-align-center">
-              <h3 className="margin-15">Програмиране!</h3>
-              <h2 className="margin-5">Всичко започна като хоби</h2>
-              <p>С изучаването на новите технологий и сблъскването с много проблеми ми ставаше все по-интересно и
-                по-любипитно. И започнах да се замислям да ли да не му дам шанс!</p>
+              <h5 className="margin-15">{t("programing")}!</h5>
+              <h6 className="margin-5">{t("started")}</h6>
+              <p>{t("startedChance")}!</p>
             </div>
           </div>
         </div>
@@ -91,10 +90,16 @@ const AboutHtml = forwardRef(({open, setOpen}, ref) => {
           <div className="overlay-box round-behavior shadowed-element border-color-gray-2px">
             <div
               className="desc flex-vertical-container-raw justify-content-center align-items-center text-align-center">
-              <h3 className="margin-15">Развитие в сферата на личните финанси и изграждане на капитали.</h3>
-              <p>Старая се всеки ден да отделям по 30мин да се образовам</p>
-              <h2 className="margin-5">Както е казал <strong>Уорън Бъфет</strong></h2>
-              <p className="margin-5">Правило №1: Никога не губете пари. Правило №2: Никога не забравяйте правило №1</p>
+              <h5 className="">{t("capital")}.</h5>
+              <h6 className="margin-5">{t("say")} <strong>{t("WarrenBuffett")}:</strong></h6>
+              <div className="flex-vertical-container-raw align-items-center text-align-center">
+                <span className="flex-horizontal-container-raw border-color-bottom-gray padding-0">{t("rule")} <p
+                  className="numbered-item padding-0"></p>1:{t("loseMoney")}.
+                </span>
+                <span className="flex-horizontal-container-raw border-color-bottom-gray ">{t("rule")} <p
+                  className="numbered-item padding-0"></p>2:{t("forgetOne")}<p className="numbered-item padding-0"></p> 1:
+                </span>
+              </div>
             </div>
           </div>
         </div>
@@ -102,10 +107,10 @@ const AboutHtml = forwardRef(({open, setOpen}, ref) => {
           <div className="overlay-box round-behavior shadowed-element border-color-gray-2px">
             <div
               className="desc flex-vertical-container-raw justify-content-center align-items-center text-align-center">
-              <h3 className="margin-15">Книги</h3>
-              <p>Тук главен виновник е едно видео което ми показа какво са парите и как те трябва да работят за мен.
-                Както е казал Робърт Кийосаки в неговата книга <strong className="motivation">"те са войници които
-                  трябва да се бият за мен докато аз спа и да печелят"</strong></p>
+              <h5 className="margin-15">{t("book")}</h5>
+              <p>{t("moneyVideo")}.</p>
+              <h6 className="">{t("say")} <strong>{t("RobertKiyosaki")}:</strong></h6>
+              <span><strong className="motivation border-color-bottom-gray ">"{t("motoRobert")}"</strong></span>
             </div>
           </div>
         </div>
