2bb99f81533541260db0a33bcecf85dd22114e0a
Dimitar <dimitar@doxita.com>
2024-07-03 08:43:06 +0300

    add Blog page

--- src/app/Blog-Page/BlogHTML.jsx
+++ src/app/Blog-Page/BlogHTML.jsx
@@ -3,11 +3,52 @@ import React from "react";
 import "./blog.scss";
 
 const BlogHtml = () => {
-  return (
-    <div className="blog-main-container text-align-center margin-40">
-      <h2> В процес на разработка!</h2>
-    </div>
-  );
+    return (
+        <div className="blog-main-container">
+            <h2> В процес на разработка!</h2>
+
+            {/*<div className="container">*/}
+            {/*    <div className="card">*/}
+            {/*        <div className="image-wrapper">*/}
+            {/*            <img alt="traveller"*/}
+            {/*                 src="https://images.unsplash.com/photo-1503457574462-bd27054394c1?crop=entropy&cs=srgb&fm=jpg&ixid=M3wzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTg4MTkwMzB8&ixlib=rb-4.0.3&q=85"/>*/}
+            {/*        </div>*/}
+            {/*        <div className="content">*/}
+            {/*            <h2>Card One</h2>*/}
+            {/*            <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown*/}
+            {/*                printer took a galley of type and scrambled it to make a type specimen book.*/}
+            {/*            </p>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*    <div className="card">*/}
+            {/*        <div className="image-wrapper">*/}
+            {/*            <img alt="traveller"*/}
+            {/*                 src="https://images.unsplash.com/photo-1628152184584-1bec8d3a3cbe?crop=entropy&cs=srgb&fm=jpg&ixid=M3wzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTg4MTk2MTJ8&ixlib=rb-4.0.3&q=85"/>*/}
+            {/*        </div>*/}
+            {/*        <div className="content">*/}
+            {/*            <h2>Card Two</h2>*/}
+            {/*            <p> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an*/}
+            {/*                unknown*/}
+            {/*                printer took a galley of type and scrambled it to make a type specimen book.*/}
+            {/*            </p>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*    <div className="card">*/}
+            {/*        <div className="image-wrapper">*/}
+            {/*            <img alt="traveller"*/}
+            {/*                 src="https://images.unsplash.com/photo-1531210194434-2bc4d24185a8?crop=entropy&cs=srgb&fm=jpg&ixid=M3wzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTg4MTk2MTJ8&ixlib=rb-4.0.3&q=85"/>*/}
+            {/*        </div>*/}
+            {/*        <div className="content">*/}
+            {/*            <h2>Card Three</h2>*/}
+            {/*            <p> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an*/}
+            {/*                unknown*/}
+            {/*                printer took a galley of type and scrambled it to make a type specimen book.*/}
+            {/*            </p>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+        </div>
+    );
 };
 
 export default BlogHtml;
