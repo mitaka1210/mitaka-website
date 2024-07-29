a3d9ada1c6f0fd1e205af559967ffd2c4b0b44c8
Dimitar <37015074+mitaka1210@users.noreply.github.com>
2024-07-29 17:06:20 +0300

    fix problem whe build

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,9 +1,11 @@
 // next-i18next.config.js
-import { resolve } from "path";
-export const i18n = {
-  locales: ["bg", "en"],
-  defaultLocale: "bg",
-  localeDetection: false,
-  react: { useSuspense: false }, //this line,
-  localePath: resolve("./public/locales")
+const path = require("path");
+module.exports = {
+  i18n: {
+    locales: ["bg", "en"],
+    defaultLocale: "bg",
+    localeDetection: false,
+    react: {useSuspense: false},//this line,
+    localePath: path.resolve("./public/locales")
+  },
 };
--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+const {i18n} = require("./next-i18next.config");
+
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -11,4 +14,4 @@ const nextConfig = {
     ];
   },
 };
-export default nextConfig;
+module.exports = nextConfig;
