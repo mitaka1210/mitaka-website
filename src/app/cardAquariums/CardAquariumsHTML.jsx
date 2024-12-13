c39e05736eefe8ff720a362f6bc3b73f0da89015
Dimitar <dimitar_dimitrov12@mail.bg>
2024-12-13 15:17:19 +0200

    add article section animation
    add article section animation

--- src/app/cardAquariums/CardAquariumsHTML.jsx
+++ src/app/cardAquariums/CardAquariumsHTML.jsx
@@ -36,6 +36,34 @@ const CardAquariumsHTML = () => {
             }
         }
     };
+    const sections = [
+        {
+            backgroundImage: "https://www.yankodesign.com/images/design_news/2019/07/desk-setups-that-maximize-productivity-part-2/Desk-Setup_12.jpg",
+            text: "Immerse yourself in a seamless experience where every touchpoint anticipates your needs. Description one.",
+            date: "2024-12-13"
+        },
+        {
+            backgroundImage: "https://i.pinimg.com/564x/34/65/b8/3465b890b11571e2c876ae74dc1b3139.jpg",
+            text: "Engage with a platform where interaction is intuitive, ensuring your journey is fluid and responsive. Description two.",
+            date: "2024-12-14"
+        },
+        {
+            backgroundImage: "https://i.pinimg.com/564x/33/d6/54/33d654e8058fdf69e160dcbd2235cdac.jpg",
+            text: "Discover our commitment to thoughtful design, prioritizing accessibility and user satisfaction. Description three.",
+            date: "2024-12-15"
+        },
+        {
+            backgroundImage: "https://i.pinimg.com/564x/39/b8/01/39b801afe355e623bcb3928b1c1b046d.jpg",
+            text: "Experience innovation at every step, with forward-thinking solutions designed to enhance your daily interactions. Description four.",
+            date: "2024-12-16"
+        },
+        {
+            backgroundImage: "https://i.pinimg.com/564x/2b/b5/48/2bb548dbf4a4c57381a7b7f736b4930b.jpg",
+            text: "Join us in embracing sustainability, where design and functionality meet eco-conscious decisions. Description five.",
+            date: "2024-12-17"
+        }
+    ];
+
     const articlesData = () => {
         if (status === 'idle') {
             dispatch(fetchArticles());
@@ -49,40 +77,51 @@ const CardAquariumsHTML = () => {
     };
     return (
         <Suspense fallback={<div>Loading...</div>}>
-            <div
-                className="height-auto flex-horizontal-container justify-content-space-evenly align-items-center flex-flow-row">
-                {
-                    articlesInfo.map((card, index) => {
-                        return <div className="flex-item card-main">
-                            <div className="py-4">
-                                <div
-                                    className="shadow-lg group container rounded-md bg-white  max-w-sm flex justify-center flex-vertical-container-raw items-center mx-auto content-div">
-                                    <div>
-                                        <div className="py-8 px-4 bg-white  rounded-b-md fd-cl group-hover:opacity-25">
-                                            <span
-                                                className="block text-lg text-gray-800 font-bold tracking-wide">{card.title}</span>
-                                            <span
-                                                className="block text-lg text-gray-800 font-bold tracking-wide">{card.create_article_date}</span>
-                                            {/*<span*/}
-                                            {/*    className="block text-gray-600 text-sm">{card.description.substring(0, 150) + " ..."}</span>*/}
-                                        </div>
-                                    </div>
-                                    <div className="absolute opacity-0 fd-sh group-hover:opacity-100">
-                                        <div className="pt-8 text-center">
-                                            <button
-                                                className="text-center rounded-lg p-4 bg-white  text-gray-700 font-bold text-lg"
-                                                onClick={() => handleClick(card.id)}>Прочети повече
-                                            </button>
-                                        </div>
-                                    </div>
+            {
+                articlesInfo.isLoading ?
+                    <div>Loading</div> :
+                    <div>
+                        <section
+                            className="flex flex-col lg:h-screen justify-center items-center overflow-scroll">
+                            <div
+                                className="prose text-gray-500 prose-sm prose-headings:font-normal prose-headings:text-xl">
+                                <div>
+                                    <h1>Animated images on hover</h1>
+                                    <p className="text-balance">Hover over the
+                                        images to see the animation.</p>
                                 </div>
                             </div>
-                        </div>
-                    })
-                }
-            </div>
+
+                            <div
+                                className="flex flex-wrap mx-auto md:flex-nowrap mt-6 border-t pt-12">
+                                {sections.map((section, index) => (
+                                    <div
+                                        key={index}
+                                        className="relative flex flex-col items-start m-1 transition ease-in-out duration-500 delay-150 transform md:w-96 md:-ml-32 md:hover:-translate-x-32 md:hover:-translate-y-8 shrink-0">
+                                        <article
+                                            className="mx-auto rounded-3xl overflow-hidden bg-cover ring-2 ring-inset ring-white/50 bg-center min-h-150 relative transform duration-500 group"
+                                            style={{backgroundImage: `url(${section.backgroundImage})`}}>
+                                            <div
+                                                className="relative h-full group-hover:bg-opacity-0 min-h-150 flex flex-wrap flex-col pt-[30rem] transform duration-500">
+                                                <div
+                                                    className="group-hover:bg-black/30 duration-500 group-hover:backdrop-blur p-8 lg:p-10 h-full justify-end flex flex-col">
+                                                    <p className="opacity-0 text-white text-sm 2xl:text-lg group-hover:opacity-80 transform duration-500">
+                                                        {section.text}
+                                                    </p>
+                                                    <p className="text-white text-xs mt-2">{section.date}</p>
+                                                </div>
+                                            </div>
+                                        </article>
+                                    </div>
+                                ))}
+                            </div>
+                        </section>
+
+                    </div>
+
+            }
         </Suspense>
     )
 }
 
-            export default CardAquariumsHTML;
+export default CardAquariumsHTML;
