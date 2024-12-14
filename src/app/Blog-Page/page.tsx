f7112fa9b12df86d212c8e6ea8d680ffdc47c5fe
Dimitar <dimitar_dimitrov12@mail.bg>
2024-12-14 19:23:19 +0200

    get section by id data and show in read page

--- src/app/Blog-Page/page.tsx
+++ src/app/Blog-Page/page.tsx
@@ -1,32 +1,32 @@
-"use client";
+'use client';
 
 import React from 'react';
 import './blog.scss';
-import dynamic from "next/dynamic";
+import dynamic from 'next/dynamic';
 
 const Page = () => {
-  const BlogHtml = dynamic(
-    () => import('./BlogHTML'),
-    {ssr: false}
-  );
-  const FooterHTML = dynamic(
-    () => import('../Footer-page/page'),
-    {ssr: false}
-  );
-  const Navigation = dynamic(
-    () => import('../Navigation-component/navigation'),
-    {ssr: false}
-  );
-  return (
-    <div className="blog-page">
-      <div
-        className="nav-bar-blog flex-vertical-container text-align-center justify-content-end ">
-        <Navigation/>
-      </div>
-      <BlogHtml/>
-      <FooterHTML/>
-    </div>
-  );
+ const BlogHtml = dynamic(
+  () => import('./BlogHTML'),
+  { ssr: false },
+ );
+ const FooterHTML = dynamic(
+  () => import('../Footer-page/page'),
+  { ssr: false },
+ );
+ const Navigation = dynamic(
+  () => import('../Navigation-component/navigation'),
+  { ssr: false },
+ );
+ return (
+  <div className="blog-page">
+   <div
+    className="nav-bar-blog flex-vertical-container text-align-center justify-content-end ">
+    <Navigation />
+   </div>
+   <BlogHtml />
+   <FooterHTML />
+  </div>
+ );
 };
 
 export default Page;
