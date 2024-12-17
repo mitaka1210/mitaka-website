5df5da433a9ad6abd045d4f51b6910680d3d1789
Dimitar <dimitar_dimitrov12@mail.bg>
2024-12-17 14:47:51 +0200

    fix build process
    fix build process

--- src/app/api/dislike/route.ts
+++ src/app/api/dislike/route.ts
@@ -4,18 +4,18 @@ import {transferTodosToArticles} from "@/app/Helper-components/checkTablesInDB/c
 
 export async function POST(req: any, res: any) {
   // изчакваме req.json() да се изпълни и да ни върне body
-  const body = await req.json()
-  console.log('pesho', body)
-  const {dislikes, id} = body;
-  try {
-    await transferTodosToArticles();
-    const client = await pool.connect();
-    const result = await client.query("UPDATE articles SET dislikes = $1 WHERE id = $2 RETURNING *", [dislikes, id]);
-    await client.end();
-    res.status(200).json({success: true, data: result.rows[0]});
-  } catch (error) {
-    return NextResponse.json({error: "Server error"});
-  }
+  // const body = await req.json()
+  // console.log('pesho', body)
+  // const {dislikes, id} = body;
+  // try {
+  //   await transferTodosToArticles();
+  //   const client = await pool.connect();
+  //   const result = await client.query("UPDATE articles SET dislikes = $1 WHERE id = $2 RETURNING *", [dislikes, id]);
+  //   await client.end();
+  //   res.status(200).json({success: true, data: result.rows[0]});
+  // } catch (error) {
+  //   return NextResponse.json({error: "Server error"});
+  // }
 
 }
 
--- src/app/api/like/route.ts
+++ src/app/api/like/route.ts
@@ -4,19 +4,19 @@ import {transferTodosToArticles} from "@/app/Helper-components/checkTablesInDB/c
 
 export async function POST(req: any, res: any) {
   // изчакваме req.json() да се изпълни и да ни върне body
-  const body = await req.json()
-  console.log('pesho', body)
-  const {likes, id} = body;
-  try {
-    await transferTodosToArticles();
-    const client = await pool.connect();
-    const result = await client.query("UPDATE articles SET likes = $1 WHERE id = $2 RETURNING *", [likes, id]);
-    await client.end();
-    res.status(200).json({success: true, data: result.rows[0]});
-    // return NextResponse.json(result);
-  } catch (error) {
-    return NextResponse.json({error: "Server error", err: error});
-  }
+  // const body = await req.json()
+  // console.log('pesho', body)
+  // const {likes, id} = body;
+  // try {
+  //   await transferTodosToArticles();
+  //   const client = await pool.connect();
+  //   const result = await client.query("UPDATE articles SET likes = $1 WHERE id = $2 RETURNING *", [likes, id]);
+  //   await client.end();
+  //   res.status(200).json({success: true, data: result.rows[0]});
+  //   // return NextResponse.json(result);
+  // } catch (error) {
+  //   return NextResponse.json({error: "Server error", err: error});
+  // }
 
 }
 
