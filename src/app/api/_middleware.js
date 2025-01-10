f03b2e5d4c0980459e6f35d49b29182ea942d979
Dimitar <dimitard185@gmail.com>
2025-01-10 17:28:11 +0200

    add new api for test vercel
    add new api for test vercel

---
+++ src/app/api/_middleware.js
@@ -0,0 +1,21 @@
+import { NextResponse } from 'next/server';
+
+export function middleware(req) {
+    const response = NextResponse.next();
+    response.headers.set('Access-Control-Allow-Origin', '*');
+    response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+        return new Response(null, {
+            status: 200,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type',
+            },
+        });
+    }
+
+    return response;
+}
