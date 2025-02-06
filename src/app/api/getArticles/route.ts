a71d45fc4b706e5380b2fba5056072568040b8fc
Dimitar <dimitard185@gmail.com>
2025-02-06 12:37:09 +0200

    refactore-read-page
    refactore-read-page fix css and mobile response

--- src/app/api/getArticles/route.ts
+++ src/app/api/getArticles/route.ts
@@ -1,27 +1,13 @@
 import { NextResponse } from 'next/server';
-import { Pool } from 'pg';
-type ArticlesData = {
- header: string,
- images_path: string,
- description: string,
- file_data: string,
- todo_id: number,
- time: string,
- date: string,
-};
-// Настройки за връзка с PostgreSQL
-const pool = new Pool({
- user: 'mitaka',
- password: 'mitaka',
- host: '192.168.55.5',
- port: 5434, // work
- database: 'postgres',
-});
+
 export async function GET() {
  try {
-  // Query to fetch all articles
-  const result = await pool.query('SELECT * FROM articles');
-  const articles = result.rows;
+  // Fetch all articles from the external endpoint
+  const response = await fetch('https://share.d-dimitrov.eu/api/articles');
+  if (!response.ok) {
+   throw new Error('Failed to fetch articles');
+  }
+  const articles = await response.json();
   // Return the fetched articles as JSON
   return NextResponse.json(articles);
  } catch (error) {
@@ -29,4 +15,3 @@ export async function GET() {
   return NextResponse.json({ error: 'Failed to fetch data' });
  }
 }
-
