6518664d0c7690c414b0edc438bdb514c4a72638
Dimitar <dimitard185@gmail.com>
2025-03-11 12:09:33 +0200

    feature/MB-mobile-login-form
    feature/MB-mobile-login-form change req to POST

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import pool from '@/database/db';
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function POST(req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== 'POST') {
         return res.status(405).json({ message: 'Method not allowed' });
     }
